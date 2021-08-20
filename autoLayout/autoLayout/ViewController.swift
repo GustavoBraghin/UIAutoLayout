a45730454c695800ab6e88e0b29914152d941741
Gustavo Braghin <gugabraghin@gmail.com>
2021-08-20 15:19:27 -0300

    First commit

---
+++ autoLayout/autoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autoLayout
+//
+//  Created by Gustavo da Silva Braghin on 17/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
